382e54c9d3c628cec8fb4dfd921b3ead65334d4a
Don Park <don@donpark.org>
2009-12-23 16:58:07 -0800

    pretty icons part2

--- src/org/donpark/whatismyip/WhatIsMyIp.java
+++ src/org/donpark/whatismyip/WhatIsMyIp.java
@@ -1,81 +1,58 @@
 package org.donpark.whatismyip;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
-import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Vector;
 
-import javax.net.SocketFactory;
-
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.message.BasicListHeaderIterator;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-
-import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class WhatIsMyIp extends ListActivity {
 	final String appTag = "WhatIsMyIp";
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
-    public void onResume() {
-    	super.onResume();
-		try {
-	    	Enumeration<NetworkInterface> interfaces = java.net.NetworkInterface.getNetworkInterfaces();
-	    	List<String> names = new ArrayList<String>();
-	    	for(;interfaces.hasMoreElements();){
-	    		NetworkInterface iface = interfaces.nextElement();
-		    	Log.i(appTag, "My interface is "+iface.getDisplayName());
-		    	String longName = iface.getDisplayName();
-	    		Enumeration<InetAddress> addrs = iface.getInetAddresses();
-		    	for(;addrs.hasMoreElements();) {
-		    		InetAddress addr = addrs.nextElement();
-		    		longName = longName + " = " + addr.toString().substring(1);
-		    		Log.i(appTag, "  IP address is "+addr);
-		    	}
-	    		names.add(longName);
-	    	}
-	    	
-//	    	Runtime runtime = Runtime.getRuntime();
-//	    	try {
-//				Process netstat = runtime.exec("cat /proc/net/route");
-//				BufferedReader out = new BufferedReader(new InputStreamReader(netstat.getInputStream()));
-//				String nextline = null;
-//				String rightline = null;
-//				while(nextline != null && rightline == null){
-//					nextline = out.readLine();
-//					//String[] tokens = nextline.split("\\s+");
-//					Log.i(appTag, nextline+" match:"+nextline.matches("00000000"));
-//				}	    	
-//				names.add(rightline);
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-	    	setListAdapter(new ArrayAdapter<String>(this,
-	    			                       android.R.layout.simple_list_item_1, 
-	    			                       names));
-	    		
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	Vector<String[]> interfaces;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	public void onResume() {
+		super.onResume();
+		interfaces = System.getNetworkInterfaces();
+		setListAdapter(new IconicAdapter());
+	}
+	
+	class IconicAdapter extends ArrayAdapter {
+		  IconicAdapter() {
+		    super(WhatIsMyIp.this, R.layout.row, interfaces.toArray());
+		  }
+		  
+		  public View getView(int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater = getLayoutInflater();
+			View row = inflater.inflate(R.layout.row, parent, false);
+			TextView label = (TextView) row.findViewById(R.id.label);
+			label.setText(interfaces.get(position)[1]);
+			ImageView icon = (ImageView) row.findViewById(R.id.icon);
+			if (interfaces.get(position)[0] == "bob") {
+				icon.setImageResource(R.drawable.telephone);
+			} else {
+				icon.setImageResource(R.drawable.server);
+			}
+			return (row);
 		}
-    }
+
+	}
 }
